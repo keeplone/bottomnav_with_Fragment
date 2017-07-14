7e2cf7d94c2a98863420aca497c9e9d4ef244de4
keeplone <keeplone@gmail.com>
2017-07-14 17:38:28 +0700

    test Branch

--- app/src/main/java/com/example/zensea/bottomnav_with_fragment/MainActivity.java
+++ app/src/main/java/com/example/zensea/bottomnav_with_fragment/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.zensea.bottomnav_with_fragment;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,36 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+        final OneFragment fragment = new OneFragment();
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, fragment);
+        fragmentTransaction.commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.navHome:
+                        fragment = new OneFragment();
+                        break;
+                    case R.id.navAlarm:
+                        fragment = new TwoFragment();
+                        break;
+                    case R.id.navNotifi:
+                        fragment = new ThreeFragment();
+                        break;
+                    case R.id.navStar:
+                        fragment = new FourFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
     }
 }
