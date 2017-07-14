4cda4461244b3bf79d793304ef884503f20f1c86
keeplone <keeplone@gmail.com>
2017-07-14 22:04:41 +0700

    Recycle_View_Card_view

--- app/src/main/java/com/example/zensea/bottomnav_with_fragment/OneFragment.java
+++ app/src/main/java/com/example/zensea/bottomnav_with_fragment/OneFragment.java
@@ -3,9 +3,14 @@ package com.example.zensea.bottomnav_with_fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.zensea.bottomnav_with_fragment.Adapter.MyAdapter;
 
 
 /**
@@ -23,7 +28,17 @@ public class OneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_one, container, false);
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv_recycler_view);
+        rv.setHasFixedSize(true);
+        MyAdapter adapter = new MyAdapter(new String[]{"Example 1", "Example 2", "Example 3", "Example 4", "Example 5"
+                , "Example 6", "Example 7"});
+        rv.setAdapter(adapter);
+
+        LinearLayoutManager lim = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(lim);
+
+        return rootView;
     }
 
 }
